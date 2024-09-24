7119b8c8d2b4188d72501fca3334efeb85451552
Adam Dunlop <Dunlop-A15@ulster.ac.uk>
2024-09-24 13:31:21 +0100

    Updated The Program To accept Inputs

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Type the first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type the secound number");
+int secoundNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secoundNumber;
+Console.WriteLine("The addition of both number is {0}", result);
+Console.ReadKey();
